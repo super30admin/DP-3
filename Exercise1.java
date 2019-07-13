b794cff13d1674ad5276e8af3d63fd0c43906095
Grover Saab <sehaj131@gmail.com>
2019-07-13 07:40:53 +0530

    complete

--- Exercise1.java
+++ Exercise1.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
         if(prices.length<=1)return 0;
-        
-        int max=0;
+        int maxP=0,min=9999;
+       
         for(int i=0;i<prices.length;i++)
         {
-           for(int j=i+1;j<prices.length;j++) 
-           { if(prices[i]<prices[j] && max<(prices[j]-prices[i])){ max=prices[j]-prices[i];}}
+            if(min>prices[i]){min=prices[i];}
+            else if(prices[i]-min>maxP){maxP=prices[i]-min;}
         }
-        return max;
+        return maxP;
     }
 }
