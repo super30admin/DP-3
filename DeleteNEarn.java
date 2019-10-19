ef3aa335667e547f31fa3f0d60c26c530773b58d
Akhilesh Borgaonkar <aborgaonkar@onemarketnetwork.com>
2019-10-19 10:33:32 -0700

    improvised prob-1 &2 solutions

--- DeleteNEarn.java
+++ DeleteNEarn.java
@@ -4,27 +4,21 @@ Problem: LC 740. Delete and Earn (DP-3)
 Approach: Using Dynamic Programming approach here to find the pattern of repeating sub-problems. 
 Time Complexity: O(n) where n is number of elements in nums[]
 Space complexity: O(1) constant
-Issue: Doesn't pass all the testcases on LC. I will re-work on this after class.
 */
 
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        Arrays.sort(nums);
-        int[][] dp = new int[nums.length+1][2];
-        int r=dp.length, c=dp[0].length;
+        int[] total = new int[100001];
+        int skipValue=0, takeValue=0;
         
-        for(int j=0; j<c; j++)
-            dp[0][j]=0;
+        for(int n : nums)
+            total[n] += n;                          //filling the total[] with sums of indexes in nums[]
         
-        for(int i=1; i<r; i++){
-            for(int j=0; j<c; j++){
-                if(j==1)
-                    dp[i][j] = dp[i-1][j-1];
-                else
-                    dp[i][j] = dp[i-1][j+1] + nums[i-1];
-            }
+        for(int i=0; i<total.length; i++){
+            int tmp = skipValue;
+            skipValue = Math.max(skipValue, takeValue); //if we skip then we take the max of previous values
+            takeValue = tmp + total[i];                 //if we take the current value then value will be previous skipValue + total of current index
         }
-        
-        return Math.max(dp[r-1][0], dp[r-1][1]);
+        return Math.max(skipValue, takeValue);          //again we take the max of last skip and take values
     }
 }
--- MinFallingPathSum.java
+++ MinFallingPathSum.java
@@ -2,40 +2,32 @@
 Author: Akhilesh Borgaonkar
 Problem: LC 931. Minimum Falling Path Sum (DP-3)
 Approach: Using Dynamic Programming approach here to find the minimum sum based on previous minimum sums in the given matrix. 
-Time Complexity: O(m*n) where m and n are rows and cols of input matrix resp.
-Space complexity: O(m * n) where m and n are rows and cols of input matrix resp.
-Issue: Gives inappropriate answer. I will re-work on this after class.
+Time Complexity: O(n^2) where n is length of given square matrix.
+Space complexity: O(1) constant because not using extra space and mutating input matrix.
 */
 
 class Solution {
     public int minFallingPathSum(int[][] A) {
-        int[][] dp = new int[A.length][A[0].length];
-        int r = dp.length, c = dp[0].length;
+        int n = A.length;
         
-        for(int j=0; j<c; j++)
-            dp[0][j] = A[0][j];
-        
-        for(int i=1; i<r; i++){
-            for(int j=0; j<c; j++){
+        for(int i=1; i<n; i++){
+            for(int j=0; j<n; j++){
                 
-                int minFromUp  = 0;
+                int minFromUp = 0;
                 if(j==0)
-                    minFromUp = Math.min(A[i-1][j],A[i-1][j+1]);
-                if(j>0 && j<c-1)
-                    minFromUp = Math.min(A[i-1][j-1], Math.min(A[i-1][j], A[i-1][j+1]));
-                if(j==c-1)
-                    minFromUp = Math.min(A[i-1][j], A[i-1][j-1]);
-                    
-                dp[i][j] = A[i][j] + minFromUp;
+                    minFromUp = Math.min(A[i-1][j],A[i-1][j+1]);        //if current index is in first column then pick the min of above and above-right
+                else if(j==(n-1))
+                    minFromUp = Math.min(A[i-1][j], A[i-1][j-1]);       //if current index is in last column then pick the min of above and above-left
+                else
+                    minFromUp = Math.min(A[i-1][j-1], Math.min(A[i-1][j], A[i-1][j+1]));    //else pick min of above-left, above and above-right
                 
-                System.out.print(dp[i][j] + ",");
+                A[i][j] += minFromUp;               //mutate the input matrix
             }
-            System.out.println(' ');
         }
         
-        int min = dp[r-1][0];
-        for (int i = 1; i < c; i++)
-            min = Math.min(min, dp[r-1][i]);
+        int min = A[n-1][0];
+        for (int i = 1; i < n; i++)             //picking the minimum from last row
+            min = Math.min(min, A[n-1][i]);
         return min;
     }
 }
