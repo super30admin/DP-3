7aff734e63ce8b4818ff0fbc651d525927eb6140
Siddharth Mehta <mehtasiddharth1991@gmail.com>
2020-01-05 20:22:35 +0530

    Fix for Time Complexity in Min Falling Path

--- MinFallingPaths.java
+++ MinFallingPaths.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(n^2)
+// Time Complexity : O(mn) 
 // Space Complexity : O(1) ---> As no explicit DS Defined
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
