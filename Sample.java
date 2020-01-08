28fd44770a22368911370e44e21740eb4f13b15d
poojakittur <gitpoojakittur@gmail.com>
2020-01-09 00:06:26 +0530

    Adding solutions to DP-3

--- Sample.java
+++ Sample.java
@@ -1,5 +1,11 @@
 // Time Complexity :
+//Problem_1 : O(max(nums))
+//Problem_2 : O(m*n)
+
 // Space Complexity :
+//Problem_1 : O(max(nums))
+//Problem_2 : O(1)
+
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
