33c28dca919b19535d2c696f1c85907d353c9002
Megha Rajam Rao <megha.rrao90@gmail.com>
2020-02-13 19:20:31 -0800

    Updated
    Updated

--- DP3_Problem1&2.py
+++ DP3_Problem1&2.py
@@ -11,8 +11,8 @@ itself in the newly built array.For the first 2 elements, we can set value as 0 
 value at n-1 and value at n-2. But am still figuring ouut the logic in white board. 
 
 -------------Problem 2---------------------------------
-Time complexity: m*O(log n) where m is amount and N is the no of elements 
-Space complexity: O(n+m) where m is amount and N is the no of elements 
+Time complexity: O(m*n) where m is amount and N is the no of elements 
+Space complexity: O(m) where m is amount and N is the no of elements 
 Executed in leetcode: Yes
 Commments: We create a dp array with size equal to amount+, with the first element as 1. Thereafter, we create two for loops to iteratively update the 
 variable with the updated amount by adding the difference between the updated amount and coin denomination. Finally, we return the last 
