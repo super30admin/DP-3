306c83ccd888c8d87c051abedda85a83cd9d12fc
ASHI RANKA <ashiranka@ASHIs-MacBook-Air.local>
2020-06-05 06:04:33 -0700

    Completed

--- DeleteAndEarn.java
+++ DeleteAndEarn.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(2n + m)
+// Time Complexity : O(n + 2m)
 // n is the number of elements in input array (given, max value : 20000)
 // m is the largest element of the input array (given, max value : 10000)
 
@@ -16,14 +16,14 @@ class Solution {
         
         //find length for temp array, as in temp array index represents numbers
         //so find max value
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) { //O(n)
             if(maxlenarray < nums[i]){
                 maxlenarray = nums[i];
             }
         }
         
         //for temp array : index = number; value = total sum of the number
-        int[] temp = new int[maxlenarray+1];
+        int[] temp = new int[maxlenarray+1];  //O(m)
         for(int i = 0; i < nums.length; i++){
             temp[nums[i]] += nums[i];
         }
@@ -32,7 +32,7 @@ class Solution {
         int dp = temp[0];
         int dp1 = Math.max(temp[0],temp[1]);  //in case of even number case eg:choose 1st and 4th
         int result = 0; 
-        for (int i = 2; i < temp.length; i++) { // find max value at every number
+        for (int i = 2; i < temp.length; i++) { // find max value at every number O(m)
             result = Math.max(dp+temp[i], dp1); //temp[i]+dp : as adj numbers cannot be added
             dp = dp1;
             dp1 = result; 
