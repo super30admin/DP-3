0dc63e9b916aa2f8012e4f07f6f12695b6c4e768
Shashank Baghel <41822011+Shashank001122@users.noreply.github.com>
2020-06-08 01:29:26 -0700

    Create MinimumFalling.py

---
+++ MinimumFalling.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minFallingPathSum(self, A: List[List[int]]) -> int:
+        for i in range(1,len(A)):
+            for j in range(len(A[0])):
+                if(i-1>0 and j+1>0 and j+1<len(A[i])and i-1<len(A[i])):
+                    A[i][j]+=min(A[i-1][j],A[i-1][j+1])
+                elif (i-1>0 and j-1>0 and j-1<len(A[i]) and i-1<len(A[i])):
+                    A[i][j]+=min(A[i-1][j],A[i-1][j-1])
+                elif i-1>0 and j-1>0 and j+1>0 and j-1<len(A[i]) and i-1<len(A[i]) and j+1<len(A[i]):
+                    A[i][j]+=min(A[i-1][j],A[i-1][j+1],A[i-1][j-1])
+        return min(A[len(A)-1])
+        
+#It's giving wrong answer on Leetcode! Please tell why? Thanks
