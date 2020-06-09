60faa0bcb42fd3f4c7202632a14a583055f3be38
Shashank Baghel <41822011+Shashank001122@users.noreply.github.com>
2020-06-08 19:08:18 -0700

    Update MinimumFalling.py

--- MinimumFalling.py
+++ MinimumFalling.py
@@ -2,12 +2,10 @@ class Solution:
     def minFallingPathSum(self, A: List[List[int]]) -> int:
         for i in range(1,len(A)):
             for j in range(len(A[0])):
-                if(i-1>0 and j+1>0 and j+1<len(A[i])and i-1<len(A[i])):
-                    A[i][j]+=min(A[i-1][j],A[i-1][j+1])
-                elif (i-1>0 and j-1>0 and j-1<len(A[i]) and i-1<len(A[i])):
-                    A[i][j]+=min(A[i-1][j],A[i-1][j-1])
-                elif i-1>0 and j-1>0 and j+1>0 and j-1<len(A[i]) and i-1<len(A[i]) and j+1<len(A[i]):
-                    A[i][j]+=min(A[i-1][j],A[i-1][j+1],A[i-1][j-1])
+                if(j==0):
+                    A[i][j]=min(A[i][j]+A[i-1][j],A[i][j]+A[i-1][j+1])
+                elif (j==len(A)-1):
+                    A[i][j]=min(A[i][j]+A[i-1][j],A[i][j]+A[i-1][j-1])
+                else:
+                    A[i][j]=min(A[i][j]+A[i-1][j],A[i][j]+A[i-1][j+1],A[i][j]+A[i-1][j-1])
         return min(A[len(A)-1])
-        
-#It's giving wrong answer on Leetcode for standard input, my answer is 11, it should be 12! Please tell why? Thanks
