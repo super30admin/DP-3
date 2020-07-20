dbeaf44753f60edc6724d80c38d5d93f6958ca63
Arpit Sharma <arpit317@gmail.com>
2020-07-19 21:51:49 -0700

    Update DeleteAndEarn.java

--- DeleteAndEarn.java
+++ DeleteAndEarn.java
@@ -1,3 +1,12 @@
+//TC: O(n), n:number of elements in nums array
+//SC: O(n)
+//Approach:
+//1. We reduce the problem to house robber, where we have to see just if either we delete this coin and earn, or we don't.
+//2. To reduce the prblem to that level, complication lies in the fact that we don't the array sorted + we don't know the neighbors of any array element.
+//3. We take another array of size equal to the maximum element in the given array, and fill this array with the given array elements, in such a way that
+// element i goes to the index i, so [1,2,2,3] becomes, [0,1,4,3].
+//4. Now, the problem is reduced and we can simply apply DP here where for every element we have 2 cases, choose or don't choose.
+
 class Solution {
     public int deleteAndEarn(int[] nums) {
         int maxNums = 0;
