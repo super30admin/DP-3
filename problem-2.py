08f060743a6f87116bc73e0ce6f7ecf154b7db2a
Basavsagar Abdagire <basavsagar.abdagire@stonybrook.edu>
2020-07-21 14:52:05 -0400

    Submitting solution for DP-3 module

--- problem-2.py
+++ problem-2.py
@@ -8,8 +8,8 @@ Approach
 """
 
 
-# Time Complexity : O(n*T) where n is the number of coins and T is the target
-# Space Complexity : O(n*T) where n is the number of coins and T is the target
+# Time Complexity : O(n*n) where n is the now rows and columns of input matrix
+# Space Complexity : O(n*n) where n is the now rows and columns of input matrix
 # Did this code successfully run on Leetcode : yes	
 # Any problem you faced while coding this : No
 
