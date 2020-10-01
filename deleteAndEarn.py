a0a6d97279382a8ab9f626f807a1c36c219e5ac1
Jeevtesh Singh <50637812+jeevy222@users.noreply.github.com>
2020-10-01 17:40:41 -0400

    Create deleteAndEarn.py

---
+++ deleteAndEarn.py
@@ -0,0 +1,20 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if  not nums:
+            return 0
+
+        freq = [0] * (max(nums)+1)
+        for n in nums:
+            freq[n] += n
+
+        dp = [0] * len(freq)
+        dp[1] = freq[1]
+        for i in range(2, len(freq)):
+            dp[i] = max(freq[i] + dp[i-2], dp[i-1])
+
+        return dp[len(freq)-1]
+        
+        #tc sc--> o(n)
+        
+        
+        
