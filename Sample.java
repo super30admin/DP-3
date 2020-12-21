55523ad12aac5326780b0aab9cee510d7a8b4cff
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-21 13:57:48 -0800

    Minimum falling path

--- Sample.java
+++ Sample.java
@@ -5,3 +5,26 @@
 
 
 // Your code here along with comments explaining your approach
+
+   public int minFallingPathSum(int[][] A) {
+        int N = A.length;
+        for (int r = N-2; r >= 0; --r) {
+            for (int c = 0; c < N; ++c) {
+                // best = min(A[r+1][c-1], A[r+1][c], A[r+1][c+1])
+                int best = A[r+1][c];
+                if (c > 0)
+                    best = Math.min(best, A[r+1][c-1]);
+                if (c+1 < N)
+                    best = Math.min(best, A[r+1][c+1]);
+                A[r][c] += best;
+            }
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int x: A[0])
+            ans = Math.min(ans, x);
+        return ans;
+    }
+
+// Time complexity : O (n^2)
+// space compelxity : O (1)
