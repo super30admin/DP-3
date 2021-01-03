071160950485c3a4d8400e49fe93d7d3cc9104dd
Anshul Atriek <atriekak@gmail.com>
2021-01-03 16:34:49 -0500

    Fixed deleteAndEarn time complexity

--- Delete and Earn.py
+++ Delete and Earn.py
@@ -6,11 +6,15 @@ class Solution:
         if len(nums) == 0:
             return 0
         
+        count_map = {}
+        for num in nums:
+            count_map[num] = count_map.get(num, 0) + 1
+        
         dp = [0 for i in range(max(nums) + 1)]
         dp[0] = 0
-        dp[1] = 1 * nums.count(1)
+        dp[1] = 1 * count_map.get(1, 0)
         
         for i in range(2, len(dp)):
-            dp[i] = max(dp[i-1], dp[i-2] + i * nums.count(i))
+            dp[i] = max(dp[i-1], dp[i-2] + i * count_map.get(i, 0))
                         
         return dp[-1]
