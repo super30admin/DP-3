ed4a41707597d4ef5eaad495fc5ca335a891cea5
Janhavi Dahihande <janhavidahihande@gmail.com>
2021-01-31 20:11:50 -0800

    Update indentation

--- delete_and_earn.py
+++ delete_and_earn.py
@@ -4,16 +4,16 @@ Space complexity: O(n)
 '''
 class Solution:
     dp = [0] * (self.getMax(nums) + 1)
-        for x in nums:
-            dp[x] += x
-            
-        c = 0 # choose
-        dc = 0 # don't choose
-        for x in dp:
-            temp = dc
-            dc = max(c, dc)  # dont choose value = max(prev choose and prev dont choose)
-            c = temp + x # choose value = sum of current number + dc of previous number
-        return max(c, dc)
+    for x in nums:
+        dp[x] += x
+        
+    c = 0 # choose
+    dc = 0 # don't choose
+    for x in dp:
+        temp = dc
+        dc = max(c, dc)  # dont choose value = max(prev choose and prev dont choose)
+        c = temp + x # choose value = sum of current number + dc of previous number
+    return max(c, dc)
 
     def getMax(self, nums): # to get the length of the array to be constrcuted. It will of the size of max number in the original array
         max = 0
