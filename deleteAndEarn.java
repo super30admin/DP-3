53ed9f20097b49d4f529ef1822dde68b197547e0
sahana263@gmail.com <svbhat@uw.edu>
2021-03-03 00:39:19 -0800

    Completed DP-3.

---
+++ deleteAndEarn.java
@@ -0,0 +1,22 @@
+// Time Complexity = O(n)
+// Space Complexity = O(1) 
+public class deleteAndEarn {
+    public int problem1(int[] nums) {
+        int[] count = new int[10001];
+        for (int x: nums) count[x]++;
+        int avoid = 0, using = 0, prev = -1;
+
+        for (int k = 0; k <= 10000; ++k) if (count[k] > 0) {
+            int m = Math.max(avoid, using);
+            if (k - 1 != prev) {
+                using = k * count[k] + m;
+                avoid = m;
+            } else {
+                using = k * count[k] + avoid;
+                avoid = m;
+            }
+            prev = k;
+        }
+        return Math.max(avoid, using);
+    }
+}
