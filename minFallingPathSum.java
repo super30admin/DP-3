d33c44d89c6ead7ee9fe8b0d078c56cc7f14a28a
savitha-baskaran <80730986+savitha-baskaran@users.noreply.github.com>
2021-03-30 23:52:14 -0400

    Create minFallingPathSum.java

---
+++ minFallingPathSum.java
@@ -0,0 +1,27 @@
+
+/*
+Run time complexity - O(M*N) where M is the length of the matrix array and N is the length of matrix[0] array
+Space complexity - O(1) we are making the change in the same array, so constant space
+*/
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        if(matrix==null || matrix.length==0)
+            return 0;
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                if(j==0)
+                    matrix[i][j]+=Math.min(matrix[i-1][j],matrix[i-1][j+1]);
+                else if(j==matrix[0].length-1)
+                    matrix[i][j]+=Math.min(matrix[i-1][j],matrix[i-1][j-1]);
+                else
+                    matrix[i][j]+=Math.min(matrix[i-1][j-1],Math.min(matrix[i-1][j],matrix[i-1][j+1]));
+            }
+        }
+      for(int i=0;i<matrix[0].length;i++)
+           min=Math.min(min,matrix[matrix.length-1][i]);
+    return min;
+    }
+}
