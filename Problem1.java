89acce4524bb29e3e41a50b4aab37e7d55160f4d
Deepali Patil <deepalipatil24@yahoo.com>
2021-06-07 18:32:12 -0700

    Added DP-3 PRs

---
+++ Problem1.java
@@ -0,0 +1,34 @@
+// 
+
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(num, max);
+        }
+        int[] arr = new int[max + 1];
+        for (int j = 0; j < nums.length; j++) {
+            int index = nums[j];
+            arr[index] += nums[j];
+        }
+        int skip = 0;
+        int take = arr[0];
+        for (int k = 0; k < arr.length; k++) {
+            int temp = skip;
+            skip = Math.max(skip, take);
+            take = temp + arr[k];
+        }
+        return Math.max(skip, take);
+    }
+}
+
+/*
+Time Complexity (Python): 
+O(NlogN), where 
+N is the length of nums. We make a single pass through the sorted keys of N, and the complexity is dominated by the sorting step.
+
+Space Complexity (Python): 
+O(N), the size of our count.
+*/
