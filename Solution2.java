c5a7f104a3a34347e559f34965437ae46c3dc627
Aditya Balasubramanyam <aditya.gnb@gmail.com>
2021-08-03 20:05:39 -0400

    Min Falling Path Sum

---
+++ Solution2.java
@@ -0,0 +1,34 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(n^2)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        if(matrix == null || matrix.length == 0){
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 1; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if(j == 0){
+                    matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i - 1][j + 1]);
+                }
+                else if(j == n - 1){
+                    matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i - 1][j - 1]);
+                }
+                else {
+                    matrix[i][j] += Math.min(matrix[i - 1][j], Math.min(matrix[i - 1][j - 1], matrix[i - 1][j + 1]));
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            min = Math.min(min, matrix[m - 1][i]);
+        }
+        return min;
+    }
+}
