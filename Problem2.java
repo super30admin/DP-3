1983c6f5ddeab6fb1437dd666d384dc10756752f
Srinivasa Rao Yedla <ysrinu15@gmail.com>
2021-08-18 00:23:01 -0700

    Dp-3

---
+++ Problem2.java
@@ -0,0 +1,27 @@
+class Problem2 {
+    public int minFallingPathSum(int[][] matrix) {
+        if(matrix == null || matrix.length == 0){
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 1; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if(j == 0){
+                    matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i - 1][j + 1]);
+                }
+                else if(j == n - 1){
+                    matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i - 1][j - 1]);
+                }
+                else {
+                    matrix[i][j] += Math.min(matrix[i - 1][j], Math.min(matrix[i - 1][j - 1], matrix[i - 1][j + 1]));
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            min = Math.min(min, matrix[m - 1][i]);
+        }
+        return min;
+    }
+}
