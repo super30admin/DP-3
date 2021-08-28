bfc9e8e75070f63f3969bee990fd9782e2b4e808
Tanvie Kirane <tanvie.kirane@gmail.com>
2021-08-28 14:24:01 -0400

    Completed Sprint DP-3

---
+++ deleteEarn.java
@@ -0,0 +1,25 @@
+// Time Complexity : O( max(maxNum, n) )
+// Space Complexity : O( max(maxNum, n) )
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        if(nums==null) return 0;
+        int max = Integer.MIN_VALUE;
+        for(int num: nums){
+            max = Math.max(max, num);
+        }
+        int arr[] = new int[max+1];
+        for(int num: nums){
+            arr[num] = arr[num] + num;
+        }
+        int skip=0; int take=0;
+        for(int i=0; i<arr.length; i++){
+            int temp = skip;
+            skip = Math.max(skip, take);
+            take = temp + arr[i];
+        }
+        return Math.max(skip, take);
+    }
+}
