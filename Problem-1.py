7c778f61b53792f0dd74a4e639c8c1c8a5c5049d
Adhish Shrivastava <adhish70@gmail.com>
2021-10-19 16:48:55 -0400

    Initial commit

---
+++ Problem-1.py
@@ -0,0 +1,9 @@
+# 740. Delete and Earn
+# https://leetcode.com/problems/delete-and-earn/
+
+# Time complexiety: 
+# Space complexiety: 
+
+class Solution:
+    def deleteAndEarn(self, nums) -> int:
+        pass
---
+++ Problem-2.py
@@ -0,0 +1,9 @@
+# 931. Minimum Falling Path Sum
+# https://leetcode.com/problems/minimum-falling-path-sum/
+
+# Time complexiety: 
+# Space complexiety: 
+
+class Solution:
+    def minFallingPathSum(self, matrix) -> int:
+        pass
