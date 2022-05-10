5b4d0ae28111b434da95eb71fbebe66365eaf14c
Rincy Soman <rincysoman@gmail.com>
2022-05-09 19:48:41 -0700

    Delete and Earn

--- Dp_DeleteAndEarn.cs
+++ Dp_DeleteAndEarn.cs
@@ -3,8 +3,32 @@ namespace Algorithms
 {
     public class Dp_DeleteAndEarn
     {
-        public Dp_DeleteAndEarn()
+        /// Time Complexity : O(N)  + O(Max(n))
+        // Space Complexity :O(Max(n)) 
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  No
+        public int DeleteAndEarn(int[] nums)
         {
+            int max = 0;
+            foreach (int num in nums)
+            {
+                max = Math.Max(max, num);
+            }
+            int[] arr = new int[max + 1];
+            foreach (int num in nums)
+            {
+                arr[num] += num;
+            }
+            int choose = arr[0];
+            int noChoose = arr[1];
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+                int temp = choose;
+                choose = Math.Max(choose, noChoose);
+                noChoose = temp + arr[i];
+            }
+            return Math.Max(choose, noChoose);
         }
     }
 }
