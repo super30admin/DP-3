f7e53288c446e1c839fd4b920c89140165bea2b0
Neha Mishra <neha79091@gmail.com>
2022-05-12 18:58:09 +0530

    minFallingPathSumUsing1DArray

--- minFallingPathSum.java
+++ minFallingPathSum.java
@@ -43,3 +43,56 @@ class Solution {
         return result;
     }
 }
+
+// ----------------USING 1D ARRAY---------------
+// Time Complexity :O(n^2)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :was confused regarding using last
+// index as it will be updated,
+// then came to know we only need one previous index and we can store it in a
+// variable
+
+// everything is same as above except we are using one dimentional dp array , as
+// mentioned above for previous index we'll store in variable
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return 0;
+        int result = Integer.MAX_VALUE;
+
+        int n = matrix.length;
+        int[] dp = new int[n];
+        // populating first row
+        for (int i = 0; i < n; i++) {
+            dp[i] = matrix[0][i];
+
+        }
+        // adding neighbours
+        int prev = 0;
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (j == 0) {
+                    int curr = dp[j];
+                    dp[j] = matrix[i][j] + Math.min(dp[j + 1], dp[j]);
+                    prev = curr;
+                } else if (j == n - 1) {
+                    int curr = dp[j];
+                    dp[j] = matrix[i][j] + Math.min(prev, dp[j]);
+                    prev = curr;
+                } else {
+                    int curr = dp[j];
+                    dp[j] = matrix[i][j] + Math.min(dp[j + 1], Math.min(prev, dp[j]));
+                    prev = curr;
+                }
+            }
+        }
+        // at the end of row, we'll update our dp matrix
+        for (int i = 0; i < n; i++) {
+            result = Math.min(result, dp[i]);
+        }
+        return result;
+    }
+}
