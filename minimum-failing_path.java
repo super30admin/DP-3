4b30cd9c364526c94486b844806a4f34cb35539b
mbashaboyina <mbashaboyina@ucdavis.edu>
2022-07-31 17:03:11 +0530

    delete_earn and minimum_failing_path

---
+++ minimum-failing_path.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int dp[] = new int[matrix.length + 1];
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            int currentRow[] = new int[matrix.length + 1];
+            for (int col = 0; col < matrix.length; col++) {
+                if (col == 0) {
+                    currentRow[col] =
+                        Math.min(dp[col], dp[col + 1]) + matrix[row][col];
+                } else if (col == matrix.length - 1) {
+                    currentRow[col] =
+                        Math.min(dp[col], dp[col - 1]) + matrix[row][col];
+                } else {
+                    currentRow[col] = Math.min(dp[col],
+                        Math.min(dp[col + 1], dp[col - 1])) + matrix[row][col];
+                }
+            }
+            dp = currentRow;
+        }
+        int minFallingSum = Integer.MAX_VALUE;
+        for (int startCol = 0; startCol < matrix.length; startCol++) {
+            minFallingSum = Math.min(minFallingSum, dp[startCol]);
+        }
+        return minFallingSum;
+    }
+}
