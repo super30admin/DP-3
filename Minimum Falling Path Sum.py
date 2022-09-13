583e79ab56d006421e8e32b36e99d5ba8de3fae9
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-09-12 17:03:12 -0700

    Completed

---
+++ Minimum Falling Path Sum.py
@@ -0,0 +1,43 @@
+# Time Complexity : O(n^2)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this : Yes, in Brute force and 2D array method I face error out of bound.
+
+
+# Your code here along with comments explaining your approach
+# Time:  O(n^2)
+# Space: O(1)
+import math
+
+
+def minFallingPathSum(matrix) -> int:
+    n = len(matrix)
+    res = list(matrix[n - 1])
+    for r in range(n - 2, -1, -1):
+        new_res = [0] * n
+        for c in range(0, n):
+            if c == 0:
+                new_res[c] = matrix[r][c] + min(res[c], res[c + 1])
+            elif c == n-1:
+                new_res[c] = matrix[r][c] + min(res[c], res[c - 1])
+            else:
+                new_res[c] = matrix[r][c] + min(res[c], res[c + 1], res[c - 1])
+        res = new_res
+                
+    return min(res)
+
+def minFallingPathSum1(matrix) -> int:
+    n = len(matrix)
+    
+    for i in range(1,n):
+        for j in range(n):
+            mini = math.inf
+            for k in range(max(0, j-1), min(n,j+2)):
+                mini = min(mini,matrix[i-1][k])
+            matrix[i][j] += mini
+            
+    return min(matrix[-1])
+
+matrix = [[2,1,3],[6,5,4],[7,8,9]]
+print(minFallingPathSum(matrix))
+print(minFallingPathSum1(matrix))
