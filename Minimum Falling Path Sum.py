83b9b5ca61ecc6820dbf693bc5da64a4dfc94f39
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-09-12 17:04:39 -0700

    Completed

--- Minimum Falling Path Sum.py
+++ Minimum Falling Path Sum.py
@@ -1,7 +1,7 @@
 # Time Complexity : O(n^2)
 # Space Complexity : O(1)
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : Yes, in Brute force and 2D array method I face error out of bound.
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Please provide solution for better understanding
 
 
 # Your code here along with comments explaining your approach
