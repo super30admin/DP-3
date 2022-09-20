6e1e8fb3f36c363bacf9fd350f468bec91e1330f
Yashnaredi <yashnaredi26@gmail.com>
2022-09-20 15:15:17 -0400

    completed dp-3

---
+++ problem2.py
@@ -0,0 +1,20 @@
+#Min Falling path sum 
+class Solution:
+    def minFallingPathSum(self, A: List[List[int]]) -> int:
+        for i in range(1,len(A)):
+            for j in range(len(A[0])):
+		
+            #edge cases are first column and last column which only have two paths from above
+                if j == 0:
+                    A[i][j]  = min((A[i][j] + A[i - 1][j]), (A[i][j] + A[i - 1][j + 1]) )
+
+                elif (j == len(A[0]) - 1):
+                    A[i][j]  = min((A[i][j] + A[i - 1][j]), (A[i][j] + A[i - 1][j - 1]) )
+
+                #every other column will have three paths coming from above
+                else:
+                    A[i][j] = min(A[i][j] + A[i - 1][j],A[i][j] + A[i - 1][j + 1], A[i][j] + A[i - 1][j - 1])
+
+        # Now that minimum falling sums for each value at the bottom row have been computer
+        # We can just take the min of the bottow row to get the smallest overall path sum 
+        return min(A[len(A) - 1])
