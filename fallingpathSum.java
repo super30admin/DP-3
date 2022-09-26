fffedcd1ac2e32c03cdc286aa010bbe09c5f89cd
Nimay Patel <60466954+Nimay-1709@users.noreply.github.com>
2022-09-25 19:13:17 -0700

    Create fallingpathSum.java

---
+++ fallingpathSum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int dp[][] = new int[matrix.length + 1][matrix.length + 1];
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (col == 0) {
+                    dp[row][col] =
+                        Math.min(dp[row + 1][col], dp[row + 1][col + 1]) + matrix[row][col];
+                } else if (col == matrix.length - 1) {
+                    dp[row][col] =
+                        Math.min(dp[row + 1][col], dp[row + 1][col - 1]) + matrix[row][col];
+                } else {
+                    dp[row][col] = Math.min(dp[row + 1][col],
+                        Math.min(dp[row + 1][col + 1], dp[row + 1][col - 1])) + matrix[row][col];
+                }
+            }
+        }
+        int minFallingSum = Integer.MAX_VALUE;
+        for (int startCol = 0; startCol < matrix.length; startCol++) {
+            minFallingSum = Math.min(minFallingSum, dp[0][startCol]);
+        }
+        return minFallingSum;
+    }
+}
