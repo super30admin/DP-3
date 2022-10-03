106a0da3ed63a04c13b62174ddea0d199683b09d
Nivedha Chokkalingam <49743648+nibfic99@users.noreply.github.com>
2022-10-03 15:01:43 -0400

    Completed Please review

---
+++ Delete and Earn.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        if(nums==null||nums.length==0)
+        {
+            return 0;
+        }
+        
+   int max=Integer.MIN_VALUE;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            max=Math.max(max,nums[i]);
+        }
+    int [] dp=new int[max+1];
+    for(int i=0;i<nums.length;i++)
+    {
+        int idx=nums[i];
+        dp[idx]=dp[idx]+nums[i];
+    }
+        int skip=0;
+        int take=dp[0];
+        
+        for(int i=0;i<max+1;i++)
+        {
+            int temp=skip;
+            skip=Math.max(skip,take);
+            take=temp+dp[i];
+        }
+        
+        return Math.max(skip,take);
+    }
+}
