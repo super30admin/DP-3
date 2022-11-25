d8d95b70f0aed3e6b3bb546b435bff0a579dfcff
Dhruv Vaghela <dhruvvaghela17@gmail.com>
2022-11-24 21:52:02 -0500

    DP-3: Minimum Falling Path Sum Complete

---
+++ MinimumFallingPathSum.py
@@ -0,0 +1,33 @@
+"""
+    Problem Statement: Minimium Falling Path Sum
+
+    Time complexity: O(1)
+
+    Space complexity: O(1)
+
+    Accepted on LeetCode? Yes! 
+
+    LeetCode Link: https://leetcode.com/problems/minimum-falling-path-sum/
+"""
+
+from typing import List
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n, m = len(matrix), len(matrix[0])
+        for i in range(n-2, -1, -1):
+            for j in range(m):
+                if j == 0:
+                    matrix[i][j] += min(matrix[i + 1][j], matrix[i + 1][j + 1])
+                elif j == m - 1:
+                    matrix[i][j] += min(matrix[i + 1][j-1], matrix[i + 1][j])
+                else:
+                    matrix[i][j] += min(matrix[i + 1][j-1], matrix[i + 1][j], matrix[i + 1][j + 1])
+        return min(matrix[0])
+
+if __name__ == "__main__":
+    sol = Solution()
+    matrix = [[2,1,3],[6,5,4],[7,8,9]]
+    
+    ret = sol.minFallingPathSum(matrix)
+    assert ret == 13
