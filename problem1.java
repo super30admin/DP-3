92cece658e38a1a804e79baf38d1af70d74fb114
Vivek Reddy Sripathi <viveksripathi5312@gmail.com>
2023-10-31 13:06:47 -0500

    dp approach

--- problem1.java
+++ problem1.java
@@ -57,10 +57,51 @@ class Solution1 {
     }
 }
 
+
+
+// Time Complexity :  O(max number+total numbers)
+// Space Complexity :  O(max number)
+// Did this code successfully run on Leetcode :  Yes
+// Any problem you faced while coding this :  No
+
+// Bottom-Up Approach : Front to back
+
+
+class Solution2 {
+    public int deleteAndEarn(int[] nums) {
+        //find out the max number in nums
+        int max= 0;
+        for(int num : nums){
+            max= Math.max(max,num);
+        }
+        // an array that will store the total value of a particular number in nums when selected.
+        // basicallty it is (frequency of the num * value of num)
+        // Range of the array would be (0--->the maximum number)
+        int[] arr= new int[max+1];
+        for(int i=0; i<nums.length; i++){
+            arr[nums[i]]+=nums[i];
+        }
+        // dp[i] will store the max power from 0 to i
+        int[] dp= new int[max+1];
+        for(int i=0; i<dp.length; i++){
+            //if we select i, then it will be sum of power until (i-2) and the current value from array.
+            //if will ignore value of (i-1)
+            int select= i-2<0 ? 0 : dp[i-2];
+            // if we don't select i, then it will be simply power until (i-1)
+            int notSelcet= i-1<0 ? 0 : dp[i-1];
+            // select the max of them and store in dp
+            dp[i]=Math.max(select+arr[i], notSelcet);
+        }
+        return dp[max];
+    }
+}
+
 public class problem1 {
     public static void main(String[] args) {
         int[] nums ={1,8,5,9,6,9,4,1,7,3,3,6,3,4,7,4,1,9,5,1,5,7,5};
         Solution1 s1= new Solution1();
         System.out.println(s1.deleteAndEarn(nums));
+        Solution2 s2= new Solution2();
+        System.out.println(s2.deleteAndEarn(nums));
     }
 }
