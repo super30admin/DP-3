50bf1fdaec47a7d9be4c47a7462ace4697db5e8e
VRwtcall <WT.VR.Project@gmail.com>
2024-01-07 21:55:52 -0600

    updated DP-3 solutions

--- min_failing_path.py
+++ min_failing_path.py
@@ -1,7 +1,7 @@
 """ Explanation: We iterate through the matrix, and then check which is the min element between (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1)
     from that element. Then, I just get the last internal matrix and get the min the value from that matrix, which should be our min failing path.
     Time: O(n^2)
-    Space: O(1)
+    Space: O(max_value)
 """
 
 
