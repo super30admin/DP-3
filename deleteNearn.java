79d56d4abf09999a5932d1d28ade52751d7fbf09
Chirag <chirag@fassur.com>
2024-02-03 19:03:57 -0800

    DP-3

---
+++ deleteNearn.java
@@ -0,0 +1,23 @@
+class deleteNearn {
+    public int deleteAndEarn(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        for(int num:nums){
+            max = Math.max(max,num);
+        }
+        int[] arr = new int[max+1];
+        for (int num:nums){
+            arr[num]+=num;
+        }
+        int prev = arr[0];
+        int curr = arr[1];
+        for(int i = 2;i<arr.length;i++){
+            int temp = curr;
+            curr = Math.max(curr,arr[i]+prev);
+            prev = temp;
+        }
+        return curr;
+    }
+}
