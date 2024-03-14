5d0fd34fd2effc3d26275f2b0b0f71ae41dc664d
Dhanashree Lale <dhanashreelale@Dhanashrees-MacBook-Pro.local>
2024-03-14 13:20:03 -0500

    completed DP-3

---
+++ MinFallingPath.java
@@ -0,0 +1,26 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+    int n = matrix.length;
+    int minPath = Integer.MAX_VALUE;
+    if(matrix == null || n == 0) return Integer.MIN_VALUE;
+    if(n == 1) return matrix[0][0];
+
+        for(int i=1; i< n; i++){
+        for(int j=0; j<n; j++){
+            if(j == 0)
+                matrix[i][j] += Math.min(matrix[i-1][0], matrix[i-1][1]);
+            else if(j == n - 1)
+                matrix[i][j] += Math.min(matrix[i-1][n-1], matrix[i-1][n-2]);
+            else
+                matrix[i][j] += Math.min(matrix[i-1][j-1], Math.min(matrix[i-1][j], matrix[i-1][j+1]));
+            if(i == n-1)
+                minPath = Math.min(minPath, matrix[i][j]);
+        }
+    }
+    return minPath;
+    }
+}
